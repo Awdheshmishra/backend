35189005a7cf0538de5e686b8056769f5c3b7ac3
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-07-09 19:38:32 +0530

    changing locations of files

--- server.js
+++ server.js
@@ -1,29 +1,9 @@
-//packages
-/* var catMe = require ('cat-me');
-console.log(catMe()); */
+//server ko start krna
 
-const express = require('express');
-const app = express()
+const app = require("./src/app")
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
+
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
 })
-
-app.get("/about", (req,res)=>{
-    res.send("about page")
-})
-app.get("/contact", (req,res)=>{
-    res.send("contact page")
-})
-app.get("/name", (req,res)=>{
-    res.send("awdhesh mishra")
-})
-
-
-app.listen(3000)//server ko start krne ke app.listen ka use kiya jata hai
-
-//REST API --> representational state trnasfer  API
-//GET -> server se data fetch karwana ho tb use krte hain
-//POST -> jab server pardata send krna ho to 
-// PATCH -> jab data server par already hai aur us data ko update karna ho 
-//DELETE -> server par data hai aur use delete karna hai tab ham delete method ka use karte hain
